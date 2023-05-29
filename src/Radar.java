b0a4034a13373dbfcf05b90bcceee6ca9a6dd9aa
MichjuX <mks2003a@gmail.com>
2023-05-29 02:36:34 +0200

    test

--- src/Radar.java
+++ src/Radar.java
@@ -1,3 +1,15 @@
-public class Radar {
+import java.util.ArrayList;
 
+public class Radar {
+    private ArrayList<AirShip> ListOfAirShips = new ArrayList<>();
+    private Map StationaryObjectsMap;
+    public void addShip(Plane plane){}
+    public void addShip(Helicopter helicopter){}
+    public void addShip(Baloon baloon){}
+    public void addShip(Glider glider){}
+    public void removeShip(int ShipID){}
+    public void genRandShip(int ShipID){}
+    public void genRandRoute(int ShipID){}
+    public void changeRoute(int ShipID, Route newRoute){}
+    public void detectForCollision(){}
 }
