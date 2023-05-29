a051405865b48e4531683c5d0f12ab480ed2a74a
Kacper Przezdziecki <kprzezdziecki0@gmail.com>
2023-05-29 02:14:00 +0200

    Test

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 
+import java.awt.event.ActionEvent;
+
 public class MyFrame extends JFrame {
     MyPanel panel;
     MyFrame(){
@@ -10,6 +12,22 @@ public class MyFrame extends JFrame {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        //t
+        createMenu(frame);
+
+        //Wyswietla okno
+        frame.pack();
+        frame.setVisible(true);
+    }
+    private void createMenu(JFrame frame) {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        JMenuItem item1 = new JMenuItem("Info");
+
+        item1.addActionListener((ActionEvent e) -> JOptionPane.showMessageDialog(frame, "Funkcjonalnosc:", "Info",
+                JOptionPane.INFORMATION_MESSAGE));
+
+        menu.add(item1);
+        menuBar.add(item1);
+        frame.setJMenuBar(menuBar);
     }
 }
